69662ace0cd7351e165094c10496958cd61197a2
Shintaro Aoi <gyoshu.bowie@gmail.com>
2021-01-06 20:36:28 +0900

    投票アプリ

---
+++ StartFirebase/UserType.swift
@@ -0,0 +1,14 @@
+//
+//  UserType.swift
+//  StartFirebase
+//
+//  Created by Owner on 2020/12/20.
+//
+
+import Foundation
+
+enum UserType {
+    case unknown
+    case new
+    case returning
+}
